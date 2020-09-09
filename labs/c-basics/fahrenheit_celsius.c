fe94096811a1372a685c0f7e1e0e5193c396880b
Diego Montiel Esparza <a01261039@itesm.mx>
2020-09-09 19:24:15 +0000

    c-basics solved
    Signed-off-by: Diego Montiel Esparza <a01261039@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define   LOWER  0       /* lower limit of table */
 #define   UPPER  300     /* upper limit */
@@ -6,12 +8,30 @@
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+float getCelsius(int f){
+	float c = ((f-32)*5)/(float)9;
+	return c;
+}
+
+int main(int argc, char* argv[])
 {
-    int fahr;
+    if(argc==2){
+	    char *q = argv[1];
+	    int n = atoi(q);
+	    printf("Fahrenheit: %d, Celcius: %61.f\n", n, getCelsius(n));
+    } else if(argc==4){
+	   char *startC = argv[1];
+	   char *endC = argv[2];
+	   char *incC = argv[3];
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	   int startI = atoi(startC);
+	   int endI = atoi(endC);
+	   int incI = atoi(incC);
 
+	   for(int i = startI; i <= endI; i = i+incI){
+		   printf("Fahrenheit: %3d, Celcius: %6.1f\n", i, getCelcius(i));
+	   }
+    }
+	   
     return 0;
 }
