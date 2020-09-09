d4cda0e461165461f5a264f19affba107d2e74e2
Diego Montiel Esparza <a01261039@itesm.mx>
2020-09-09 21:24:30 +0000

    solved word reverser
    Signed-off-by: Diego Montiel Esparza <a01261039@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -2,5 +2,20 @@
 
 int main(){
     // Place your magic here
+    char c;
+    char word[100]="";
+    int count = 0;
+    while((c = getchar()) != EOF){
+	    if(c == '\n'){
+		    for(int i = count; i >= 0; i--){
+			    printf("%c", words[i]);
+		    }
+		    printf("/n");
+		    memset(words, 0, sizeof words);
+	    } else {
+		    words[count] = c;
+		    count++;
+	    }
+    }
     return 0;
 }
