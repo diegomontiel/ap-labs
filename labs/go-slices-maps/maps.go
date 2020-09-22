ebfb6e2170d32f71e7d7e82d93a3d4b3d81ee707
Diego Montiel Esparza <a01261039@itesm.mx>
2020-09-22 04:28:52 +0000

    Solved slices and maps in Go
    Signed-off-by: Diego Montiel Esparza <a01261039@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+
+	res := make(map[string]int)
+	word := strings.Fields(s)
+
+	for _, w := range word {
+		res[w]++
+
+	}
+	return res
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,21 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var picture[][]uint8
+	picture = make([][]uint8, dy)
+	for y := range picture {
+		picture[y] = make([]uint8, dx)
+		for x := range picture[y] {
+			//picture[y][x] = uint8((x+y)/2) //blue gradient
+			//picture[y][x] = uint8(x*y) //cuadratic pattern
+			picture[y][x] = uint8(x^y) //square pattern
+		}
+	}
+	return picture
+}
+
 func main() {
+	pic.Show(Pic)
 }
