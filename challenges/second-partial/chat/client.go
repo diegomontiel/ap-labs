222cc407e865e70725cc766a017a8259da6cd6ea
Diego Montiel Esparza <a01261039@itesm.mx>
2020-10-20 21:33:50 +0000

    Solved second partial
    Signed-off-by: Diego Montiel Esparza <a01261039@itesm.mx>

--- challenges/second-partial/chat/client.go
+++ challenges/second-partial/chat/client.go
@@ -7,31 +7,36 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 )
-
-//!+
+//!+main
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8000")
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) != 5 {
+		log.Fatal("Bad arguments")
+	} else {
+		conn, err := net.Dial("tcp", os.Args[4])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(conn, os.Args[2])
+		done := make(chan struct{})
+		go func() {
+			io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+			log.Println("done")
+			done <- struct{}{} // signal the main goroutine
+		}()
+		mustCopy(conn, os.Stdin)
+		conn.Close()
+		<-done // wait for background goroutine to finish
 	}
-	done := make(chan struct{})
-	go func() {
-		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
-		log.Println("done")
-		done <- struct{}{} // signal the main goroutine
-	}()
-	mustCopy(conn, os.Stdin)
-	conn.Close()
-	<-done // wait for background goroutine to finish
+
 }
 
-//!-
-
+//!-main
 func mustCopy(dst io.Writer, src io.Reader) {
 	if _, err := io.Copy(dst, src); err != nil {
 		log.Fatal(err)
--- challenges/second-partial/chat/server.go
+++ challenges/second-partial/chat/server.go
@@ -12,19 +12,29 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"time"
+	"strings"
 )
 
 //!+broadcaster
-type client chan<- string // an outgoing message channel
+type msgChan chan<- string // an outgoing message channel
+type client struct {
+	channel msgChan
+	name string
+	ip string
+	conn net.Conn
+	admin bool
+}
 
 var (
 	entering = make(chan client)
-	leaving  = make(chan client)
-	messages = make(chan string) // all incoming client messages
+	leaving  = make(chan msgChan)
+	messages = make(chan string)
+	clients  = make(map[msgChan]*client) //connected clients
 )
 
 func broadcaster() {
-	clients := make(map[client]bool) // all connected clients
 	for {
 		select {
 		case msg := <-messages:
@@ -34,8 +44,11 @@ func broadcaster() {
 				cli <- msg
 			}
 
-		case cli := <-entering:
-			clients[cli] = true
+		case clientData := <-entering:
+			if len(clients) == 0 {
+				clientData.admin = true
+			}
+			clients[clientData.channel] = &clientData
 
 		case cli := <-leaving:
 			delete(clients, cli)
@@ -48,41 +61,167 @@ func broadcaster() {
 
 //!+handleConn
 func handleConn(conn net.Conn) {
-	ch := make(chan string) // outgoing client messages
-	go clientWriter(conn, ch)
-
-	who := conn.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
-	entering <- ch
-
+	var userAvailable = false
 	input := bufio.NewScanner(conn)
-	for input.Scan() {
-		messages <- who + ": " + input.Text()
+	input.Scan()
+	who := input.Text()
+	for _,c := range clients {
+		if c.name == who {
+			userAvailable = true
+			fmt.Fprintln(conn, "Username already taken")
+			conn.Close()
+			break;
+		}
 	}
-	// NOTE: ignoring potential errors from input.Err()
 
-	leaving <- ch
-	messages <- who + " has left"
-	conn.Close()
+	if !userAvailable {
+		ch := make(chan string) // outgoing client messages
+		go clientText(conn, ch)
+		ip := conn.RemoteAddr().String()
+
+		ch <- serverInfo + "Welcome to the Simple IRC Server"
+		ch <- serverInfo + "Your user [" + who + "] is successfully logged"
+		fmt.Println(serverInfo + "New connected user [" + who + "]")
+		messages <- serverInfo + "New connected user [" + who + "]"
+
+		entering <- client{ch, who, ip, conn, false}
+
+		if clients[ch].admin == true{
+			ch <- serverInfo + "Congrats, you were the first user."
+			promote(who, ch)
+		}
+
+		for input.Scan() { // Incoming client messages
+			msg := input.Text()
+			if msg != "" {
+				if msg[0] == '/' {
+					values := strings.Split(msg, " ")
+					switch values[0] {
+
+				//USER INPUT (parameter check requiered)
+					case "/msg": //msg to user
+						if len(values) < 3 {
+							ch <- serverInfo + "Incomplete parameters"
+						} else {
+							var check = false
+							for _,p := range clients {
+								if p.name == values[1] {
+									check = true
+									p.channel <- who + " (direct) > " + msg[6+len(p.name):]
+									break;
+								}
+							}
+							if !check {
+								ch <- serverInfo + "Username not found, message failed."
+							}
+						}
+
+					case "/user": //display user information
+						if len(values) != 2 {
+							ch <- serverInfo + "Error, correct usage is: /user <username>"
+						} else {
+							var check = false
+							for _,p := range clients {
+								if p.name == values[1] {
+									check = true
+									ch <- serverInfo + "username: " + p.name + ", IP: " + p.ip
+									break;
+								}
+							}
+							if !check {
+								ch <- serverInfo + "Username not found, failed to display info"
+							}
+						}
+
+					case "/kick": //kick any user 
+						if clients[ch].admin {
+							if len(values) != 2 {
+								ch <- serverInfo + "Error, correct usage is: /kick <username>"
+							} else {
+								var check = false
+								for _,p := range clients {
+									if p.name == values[1] {
+										check = true
+										p.channel <- serverInfo + "You're kicked from this channel"
+										leaving <- p.channel
+										fmt.Println(serverInfo + p.name + " was kicked")
+										messages <- serverInfo + p.name + " was kicked from channel"
+										p.conn.Close()
+										break;
+									}
+								}
+								if !check {
+									ch <- serverInfo + "Username not found, failed to kick"
+								}
+							}
+						} else {
+							ch <- serverInfo + "Error: only the admin can kick"
+						}
+
+				//NO PARAMS
+					case "/users": //list all users
+						var userNames string
+						for _,c := range clients{
+							userNames += c.name + ", "
+						}
+						ch <- serverInfo + userNames[:len(userNames)-2]
+
+					case "/time": //display current time
+						location,_ := time.LoadLocation("Local")
+						local := location.String()
+						if local == "Local"{
+							curr,_ := time.LoadLocation("America/Mexico_City")
+							local = curr.String()
+						}
+						ch <- serverInfo + "Local Time: " + local + " " + time.Now().Format("15:04")
+
+
+					default:
+						ch <- serverInfo + "Error, unknown command"
+					}
+				} else { // Message
+					messages <- who + " > " + msg
+				}
+			}
+		}
+		if clients[ch] != nil {
+			leaving <- ch
+			fmt.Println(serverInfo + "[" + who + "] left")
+			messages <- serverInfo + "[" + who + "] left channel"
+			conn.Close()
+		}
+	}
 }
 
-func clientWriter(conn net.Conn, ch <-chan string) {
+func clientText(conn net.Conn, ch <-chan string) {
 	for msg := range ch {
 		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
 	}
 }
 
+func promote(who string, cli msgChan) {
+	fmt.Println(serverInfo + "["+ who + "] was promoted as the channel ADMIN")
+	clients[cli].admin = true
+	cli <- serverInfo + "You're the new IRC Server ADMIN"
+}
 //!-handleConn
 
 //!+main
+
+var serverInfo string
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	if len(os.Args) != 5 {
+		log.Fatal("Error in parameters, usage: go run client.go -host [host] -port [port]")
+	}
+	serverInfo = "irc-server > "
+	server := os.Args[2] + ":" + os.Args[4]
+	listener, err := net.Listen("tcp", server)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	fmt.Println(serverInfo + "Simple IRC Server started at " + server)
 	go broadcaster()
+	fmt.Println(serverInfo + "Ready for receiving new clients")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
