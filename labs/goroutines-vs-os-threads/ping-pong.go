5d6e06541e9a8e816718e638013def3a4ed2bc50
Diego Montiel Esparza <a01261039@itesm.mx>
2020-11-11 04:40:50 +0000

    Goroutines lab solved
    Signed-off-by: Diego Montiel Esparza <a01261039@itesm.mx>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -1,11 +1,34 @@
 package main
 
-import (
+import(
 	"fmt"
+	"time"
 )
+var comms int
 
-func main() {
-	var commsPerSecond int
-
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+func ping(in chan bool, out chan bool){
+	for {
+		out <- true
+		<- in
+		comms++
+	}
+}
+func pong(in chan bool, out chan bool){
+	for {
+		<- in
+		comms++
+		out <- false
+	}
+}
+func main(){
+	ch1 := make(chan bool)
+	ch2 := make(chan bool)
+	fmt.Println("Welcome to Ping Pong")
+	go ping(ch1, ch2)
+	go pong(ch2, ch1)
+	for i:=0; i<10; i++ {
+		time.Sleep(time.Second)
+		fmt.Println("Comunications per second:",comms)
+		comms = 0
+	}
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -1,12 +1,27 @@
 package main
 
-import (
+import(
 	"fmt"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+func routine(n int, returnCH chan time.Time){
+	if n > 1 {
+		getCH := make(chan time.Time)
+		go routine(n - 1, getCH)
+		transitTime := <- getCH
+		returnCH <- transitTime
+		return
+	}
+	returnCH <- time.Now()
+}
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func main(){
+	var maxStages int
+    maxStages=100000
+	getCH := make(chan time.Time)
+	go routine(maxStages, getCH)
+	transitTime := <- getCH
+    fmt.Println("Maximum number of pipeline stages   : ", maxStages)
+	fmt.Println("Time to transit trough the pipeline : ", time.Since(transitTime))
 }
